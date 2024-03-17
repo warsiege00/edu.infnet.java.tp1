12a1cba8287a41778d8b8df77a89973059df3c70
Matheus <matheus.almeida053@gmail.com>
2024-03-17 01:28:19 -0300

    feat: Refatorando classe produto e configurações para criar e acessar BD

--- src/main/java/edu/infnet/matheuspiraine/model/domain/Produto.java
+++ src/main/java/edu/infnet/matheuspiraine/model/domain/Produto.java
@@ -1,11 +1,23 @@
 package edu.infnet.matheuspiraine.model.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Produto {
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private Double preco;
-    private Integer quantidadeEstoque;
 
+    public Produto(){
+        
+    }
+   
     public Integer getId() {
         return id;
     }
@@ -13,35 +25,19 @@ public class Produto {
         this.id = id;
     }
     public void setNome(String nome) {
-        if (nome == null || nome.trim().isEmpty()) {
-            this.nome = "";
-        }else {
-            this.nome = nome;
-        }
-
+        this.nome = nome;
     }
     public String getNome() { return nome;}
+
     public void setPreco(Double preco) {
-        if (preco <= 0) {
-            this.preco = 0.0;
-        }
         this.preco = preco;
     }
     public Double getPreco() {return preco;}
-    public void setQuantidadeEstoque(Integer quantidadeEstoque) {
-        this.quantidadeEstoque = Math.max(quantidadeEstoque, 0);
 
-    }
-    public Integer getQuantidadeEstoque() { return quantidadeEstoque;}
     public void imprimirInformacoes() {
         System.out.println("ID: " + id);
         System.out.println("Nome: " + nome);
         System.out.println("Preço: " + preco);
-        System.out.println("Quantidade em estoque: " + quantidadeEstoque);
-    }
-
-    public Double calcularValorTotalEstoque() {
-        return preco * quantidadeEstoque;
     }
 }
 
